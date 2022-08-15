4a0b5277898b703894ab1c9cd58cf5f5ca73cad8
Alex <wild6158@gmail.com>
2022-08-15 10:00:21 +0300

    Initial Commit

---
+++ iOS6-HW26-Alexander.Strelkov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS6-HW26-Alexander.Strelkov
+//
+//  Created by Alexandr Strelkov on 15.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
