bb684c193c6b6f9084dcfbf87473d9d8f7575232
Alex <wild6158@gmail.com>
2022-08-19 15:21:23 +0300

    Made press button logic with UIAlert
    Made press button logic with UIAlert to add users to tableView

--- iOS6-HW26-Alexander.Strelkov/Sources/Scenes/View/UsersViewController.swift
+++ iOS6-HW26-Alexander.Strelkov/Sources/Scenes/View/UsersViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class UsersViewController: UIViewController {
     
+    var usersArray = ["Alexander Strelkov", "Ivan Antipov"]
+    
     @IBOutlet weak var textField: UITextField!
 
     @IBOutlet weak var tableView: UITableView!
 
+    @IBOutlet weak var pressButton: UIButton!
+    
     @IBAction func textTypedField(_ sender: UITextField) {
         
     }
     
     @IBAction func buttonPressed(_ sender: UIButton) {
+        let alert = UIAlertController(title: "\(self.textField.text ?? "") will be added", message: "", preferredStyle: .alert)
         
+        let action = UIAlertAction(title: "Confirm", style: .default) { (action) in
+            print(self.textField.text ?? "")
+            self.usersArray.append(self.textField.text ?? "")
+            self.tableView.reloadData()
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
@@ -29,12 +41,13 @@ class UsersViewController: UIViewController {
 
 extension UsersViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return usersArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "usersCell", for: indexPath)
-        cell.textLabel?.text = "Cell number \(indexPath.row)"
+        
+        cell.textLabel?.text = usersArray[indexPath.row]
         return cell
     }
     
