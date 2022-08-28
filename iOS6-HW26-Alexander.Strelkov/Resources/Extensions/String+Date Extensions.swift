5c5de32624374ac95ed8edabd90f536ac4c25f7b
Alex <wild6158@gmail.com>
2022-08-28 18:54:55 +0300

    deleted public in extensions

--- iOS6-HW26-Alexander.Strelkov/Resources/Extensions/String+Date Extensions.swift
+++ iOS6-HW26-Alexander.Strelkov/Resources/Extensions/String+Date Extensions.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 extension String {
-    public func convertToDate() -> Date? {
+    func convertToDate() -> Date? {
         let formatter = DateFormatter()
         formatter.dateFormat = "dd.MM.yyyy"
         return formatter.date(from: self)
